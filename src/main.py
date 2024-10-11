a3d3ccfc8025d7b98e21f22a3e2326e35408962d
libewa <libewa.git@icloud.com>
2024-10-11 09:51:18 +0200

    loop screen

--- src/main.py
+++ src/main.py
@@ -9,9 +9,9 @@ color_bg = pygame.Color(0xdf, 0xaf, 0xf)
 color_snake = pygame.Color(0xff, 0xdf, 0xaf)
 color_item = pygame.Color(0xaf, 0xff, 0xdf)
 
-pos_snake = [(120, 120), (120, 136), (120, 152), (136, 152)]
+pos_snake = [(120, 120), (120, 136), (120, 152), (136, 152), (152, 152)]
 
-dir = "up"
+current_dir = "up"
 
 while running:
     for event in pygame.event.get():
@@ -19,16 +19,16 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        dir = "up"
-    if keys[pygame.K_DOWN]:
-        dir = "down"
-    if keys[pygame.K_LEFT]:
-        dir = "left"
-    if keys[pygame.K_RIGHT]:
-        dir = "right"
+    if keys[pygame.K_UP] and current_dir != "down":
+        current_dir = "up"
+    if keys[pygame.K_DOWN] and current_dir != "up":
+        current_dir = "down"
+    if keys[pygame.K_LEFT] and current_dir != "right":
+        current_dir = "left"
+    if keys[pygame.K_RIGHT] and current_dir != "left":
+        current_dir = "right"
     
-    match dir:
+    match current_dir:
         case "up":
             pos_snake.insert(0, (pos_snake[0][0], pos_snake[0][1] - 16))
         case "down":
@@ -37,11 +37,24 @@ while running:
             pos_snake.insert(0, (pos_snake[0][0] - 16, pos_snake[0][1]))
         case "right":
             pos_snake.insert(0, (pos_snake[0][0] + 16, pos_snake[0][1]))
-    
+
+    if pos_snake[0][0] > 248:
+        pos_snake[0] = (8, pos_snake[0][1])
+    if pos_snake[0][1] > 248:
+        pos_snake[0] = (pos_snake[0][0], 8)
+    if pos_snake[0][0] < 8:
+        pos_snake[0] = (248, pos_snake[0][1])
+    if pos_snake[0][1] < 8:
+        pos_snake[0] = (pos_snake[0][0], 248)
+    print(pos_snake[0])
     pos_snake.pop()
 
     screen.fill(color_bg)
     pygame.draw.circle(screen, color_snake, pos_snake[0], 8)
+    for e in pos_snake:
+        if pos_snake.index(e) == 0:
+            continue
+        pygame.draw.rect(screen, color_snake, (e[0]-8, e[1]-8, 16, 16))
 
     pygame.display.flip()
 
